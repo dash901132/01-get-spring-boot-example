f607537151cf70a259d526c9790651fa0f80d2e9
dash901132 <you@example.com>
2021-03-12 13:07:37 +0530

    creating new repo on git

---
+++ src/test/java/com/git/poc/GitpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitpracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
