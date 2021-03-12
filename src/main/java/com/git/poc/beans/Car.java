a9787751f9950a3d68f9d114666a0a9a9561c1af
dash901132 <you@example.com>
2021-03-12 13:35:18 +0530

    did  changes in engine

--- src/main/java/com/git/poc/beans/Car.java
+++ src/main/java/com/git/poc/beans/Car.java
@@ -1,4 +1,19 @@
 package com.git.poc.beans;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Component
 public class Car {
+    @Autowired
+    Engine engine;
+   static{
+       System.out.println("this car class");
+   }
+
+   public void start(){
+       engine.engine();
+       System.out.println("get engine for starting car");
+   }
 }
--- src/main/java/com/git/poc/beans/Engine.java
+++ src/main/java/com/git/poc/beans/Engine.java
@@ -1,5 +1,8 @@
 package com.git.poc.beans;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Engine {
     public void engine(){
         System.out.println("car need engine for start");
