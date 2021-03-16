800c3b7777ea1af32ce5ac779c6f55b02278e2b0
dash901132 <you@example.com>
2021-03-16 15:03:11 +0530

    merge conflit senario

--- src/main/java/com/git/poc/GitpracticeApplication.java
+++ src/main/java/com/git/poc/GitpracticeApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitpracticeApplication {
 	public static final String REST_URL="";
+	public static int a=10;
 
     public static void main(String[] args) {
         SpringApplication.run(GitpracticeApplication.class, args);
