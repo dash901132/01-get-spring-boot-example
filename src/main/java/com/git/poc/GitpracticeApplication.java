881f232e6abccd836cf4f07a1a16fc82a5fcad6a
dash901132 <you@example.com>
2021-03-16 15:08:36 +0530

    merge conflict reslove scenario

--- src/main/java/com/git/poc/GitpracticeApplication.java
+++ src/main/java/com/git/poc/GitpracticeApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitpracticeApplication {
 	public static final String REST_URL="";
+	String name="Dashrath";
 
     public static void main(String[] args) {
         SpringApplication.run(GitpracticeApplication.class, args);
